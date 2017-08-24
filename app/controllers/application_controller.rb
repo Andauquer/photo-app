86ba1967a0661a11106766ca2f50b32c7162f1be
Andauquer <geba23@hotmail.com>
2017-08-24 01:09:09 +0000

    Add devise and styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  # Para ingresar a la aplicacion primero el usuario debe ser autenticado.
+  before_action :authenticate_user!
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
+  
+  # Definimos que before_action, declarado en application_controller no se tomara en cuenta
+  # para la accion index.
+  skip_before_action :authenticate_user!, only: [:index]
+  
   def index
+  
   end
+
 end
